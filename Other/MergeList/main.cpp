5bdb9731c41bf9e506d23cb75afa59915f38c118
VicenteMcMahon <108867242+VicenteMcMahon@users.noreply.github.com>
2024-01-29 15:20:12 +0000

    Added merge list.

---
+++ Other/MergeList/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#define LENGTH(x) sizeof(x) / sizeof(x[0])
+
+int main(void) {
+    // Creates the list.
+    int list1[3] = {1, 128, 256};
+    int list2[6] = {-1, 6, 9, 10, 15, 19};
+    // Creates the merged list variable.
+    int mergedList[LENGTH(list1)+LENGTH(list2)];
+    // The index of the merged list.
+    unsigned int index = 0;
+    // The index of the first list.
+    unsigned int i = 0;
+    // The index of the second list.
+    unsigned int j = 0;
+    // Loops through until all items of merged list are added.
+    while (index < LENGTH(list1)+LENGTH(list2)) {
+        // If list1 item > list2 item and some checks to make sure that the index is in range.
+        if (i >= LENGTH(list1) || (j <  LENGTH(list2) && list1[i] > list2[j])) {
+            mergedList[index] = list2[j];
+            // Increments the second list number.
+            j++;
+        } else {
+            mergedList[index] = list1[i];
+            // Increments the first list number.
+            i++;
+        }
+        index++;
+    }
+    // Outputs the list.
+    for (int a = 0; a < LENGTH(list1)+LENGTH(list2); a++) {
+        std::cout << mergedList[a] << "\n";
+    }
+}
